06cc3145116c37d5e17519e0324c8a1980cc4a6e
Marbin-Jeanpoll <marbin_26_06@hotmail.com>
2023-03-28 20:36:09 -0500

    IniciandoProyeceto

---
+++ Ejercicio1_VHS/Ejercicio1_VHS/Armas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1_VHS
+{
+    class Armas
+    {
+        string name;
+        float damage;
+        float attackSpeed;
+
+        public void DamagePerSecond()
+        {
+            float damageForSecond = (damage / 2) * attackSpeed; 
+        }
+    }
+}
